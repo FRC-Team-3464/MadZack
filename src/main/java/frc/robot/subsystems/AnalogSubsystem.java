763a3dcdc6c35e387f876679b510ae59f60b76cd
Acer1 <Acer1>
2023-02-02 17:19:33 -0500

    IDK what's happening

---
+++ src/main/java/frc/robot/subsystems/AnalogSubsystem.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AnalogSubsystem extends SubsystemBase {
+  /** Creates a new AnalogSubsystem. */
+  // AnalogPotentiometer analogPot = new AnalogPotentiometer(chanel:0, );
+  public AnalogSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
